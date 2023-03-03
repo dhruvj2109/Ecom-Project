4091de8349eb715207963c37ec9fe36e030326aa
Dhruv Jindal <dhruvjindal2109@gmail.com>
2023-03-03 20:59:14 +0530

    change in Channel

--- src/Router/Channel.js
+++ src/Router/Channel.js
@@ -3,10 +3,19 @@ import { useLocation } from "react-router-dom";
 
 const Channel = () => {
     const location = useLocation();
+    useEffect(() => {
+        first;
+
+        return () => {
+            second;
+        };
+    }, [third]);
+
     console.log(location);
     return (
         <div>
             <h1>{location.pathname}</h1>
+            <h1>Hello this is for testing</h1>
         </div>
     );
 };
